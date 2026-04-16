1aaed7ebb9126fa67c2cc4d1daf0520dfa71cc6b
sravan875 <sravankumarkona875@gmail.com>
2026-04-16 14:34:09 +0530

    Sum Arguments

---
+++ SumArguments.java
@@ -0,0 +1,25 @@
+public class SumArguments {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("No command-line arguments provided!");
+            return;
+        }
+
+        int sum = 0;
+        int invalidCount = 0;
+
+        for (String arg : args) {
+            try {
+                int num = Integer.parseInt(arg);
+                sum += num;
+            } catch (NumberFormatException e) {
+                invalidCount++;
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Invalid inputs = " + invalidCount);
+    }
+}
